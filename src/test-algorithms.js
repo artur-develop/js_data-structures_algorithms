c8a4e98ea754b549e5a508285ac7305252b017e3
Artur Yurkevich <artur.urkevich@gmail.com>
2019-08-03 20:31:20 +0300

    PriorityQueue, BinaryTree

--- src/test-algorithms.js
+++ src/test-algorithms.js
@@ -4,6 +4,8 @@ import Stack from './data-structures/Stack/Stack';
 import Queue from './data-structures/Queue/Queue';
 import HashTable from './data-structures/HashTable/HashTable';
 import BinaryHeap from './data-structures/BinaryHeap/BinaryHeap';
+import PriorityQueue from './data-structures/PriorityQueue/PriorityQueue';
+import BinaryTree from './data-structures/BinaryTree/BinaryTree';
 
 console.log('========== LinkedList ==========');
 const linkedList = new LinkedList();
@@ -79,5 +81,38 @@ const binaryHeap = new BinaryHeap((a, b) => {
 // https://codepen.io/beaucarnes/pen/JNvENQ?editors=0010
 binaryHeap.build([20, 19, 17, 13, 15, 8, 5, 11, 9, 10]);
 binaryHeap.add(5);
-
 binaryHeap.print();
+
+
+
+// PriorityQueue
+console.log('========== PriorityQueue ==========');
+const priorityQueue = new PriorityQueue();
+console.log(priorityQueue.isEmpty()); // testing isEmpty and front on an empty queue. return true
+console.log(priorityQueue.front()); // returns "No elements in Queue"
+// adding elements to the queue
+priorityQueue.enqueue("Sumit", 2);
+priorityQueue.enqueue("Gourav", 1);
+priorityQueue.enqueue("Piyush", 1);
+priorityQueue.enqueue("Sunny", 2);
+priorityQueue.enqueue("Sheru", 3);
+console.log(priorityQueue.printPQueue()); // prints [Gourav Piyush Sumit Sunny Sheru]
+console.log(priorityQueue.front().element); // prints Gourav
+console.log(priorityQueue.rear().element); // pritns Sheru
+console.log(priorityQueue.dequeue().element); // removes Gouurav. priorityQueue contains [Piyush Sumit Sunny Sheru]
+priorityQueue.enqueue("Sunil", 2); // Adding another element to the queue
+console.log(priorityQueue.printPQueue()); // prints [Piyush Sumit Sunny Sunil Sheru]
+
+
+// BinarySearchTree
+console.log('========== BinarySearchTree ==========');
+const binaryTree = new BinaryTree();
+binaryTree.add(50);
+binaryTree.add(12);
+binaryTree.add(26);
+binaryTree.add(50);
+binaryTree.add(19);
+binaryTree.add(88);
+binaryTree.add(55);
+
+
